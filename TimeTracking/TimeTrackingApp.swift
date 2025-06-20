5c4abcf943b2e4f2a8aa89afa6f6d895711e90a0
Thibaut Izard <thibaut.izard@gmail.com>
2025-06-20 16:58:19 +0200

    Initial Commit

---
+++ TimeTracking/TimeTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingApp.swift
+//  TimeTracking
+//
+//  Created by Thibaut Izard on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeTrackingTests/TimeTrackingTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingTests.swift
+//  TimeTrackingTests
+//
+//  Created by Thibaut Izard on 20/06/2025.
+//
+
+import Testing
+@testable import TimeTracking
+
+struct TimeTrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
