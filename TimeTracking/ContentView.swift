f031bf8b6064d31b6ff467037513c898b005a748
Thibaut Izard <thibaut.izard@gmail.com>
2025-06-21 11:05:22 +0200

    feat: Change buttons color

--- TimeTracking/ContentView.swift
+++ TimeTracking/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var timerRunning = false
     @State private var timer: Timer?
     
+    
     var body: some View {
         VStack(spacing:30) {
             Text("Time tracking")
@@ -26,19 +27,30 @@ struct ContentView: View {
                 Button("Start timer") {
                     start()
                 }
+                .padding()
+                .font(.system(size: 16, weight: .bold))
+                .foregroundColor(Color.white)
+                .background(Color.blue, in: RoundedRectangle(cornerRadius: 8))
                 .disabled(timerRunning)
-                
+                .opacity(timerRunning ? 0.5 : 1)
                 Button("Stop timer") {
                     stop()
                 }.disabled(!timerRunning)
-                
-                Button("Reset") {
-                    reset()
-                }.disabled(timerRunning)
             }
+            Button("Reset") {
+                reset()
+            }
+            .font(.system(size: 16, weight: .bold))
+            .padding()
+            .foregroundColor(Color.white)
+            .background(Color.red, in: RoundedRectangle(cornerRadius: 8))
+            .opacity(timerRunning || elapsedTime == 0 ? 0.5 : 1)
+            .shadow(color: timerRunning || elapsedTime == 0 ? .clear : .gray , radius: 5, x: 0, y: 2)
+            .disabled(timerRunning)
         }
         
     }
+    
     
     // Functions
     
