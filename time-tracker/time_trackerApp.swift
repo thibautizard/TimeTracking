41e68f3f0749f3a3bae23eb9c2cb7d93df59aca2
Thibaut Izard <thibaut.izard@gmail.com>
2025-08-16 18:45:07 +0200

    Initial Commit

---
+++ time-tracker/time_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  time_trackerApp.swift
+//  time-tracker
+//
+//  Created by Thibaut Izard on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct time_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ time-trackerTests/time_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  time_trackerTests.swift
+//  time-trackerTests
+//
+//  Created by Thibaut Izard on 16/08/2025.
+//
+
+import Testing
+@testable import time_tracker
+
+struct time_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
